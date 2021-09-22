792f2226e62cf8731075d206e01e7d833261f6df
rpera1991 <66051954+rpera1991@users.noreply.github.com>
2021-09-21 21:52:31 -0300

    LastUpdate Commit

--- pages/account.js
+++ pages/account.js
@@ -5,6 +5,10 @@ module.exports = {
       selector: '//div[contains(@class,"container")]//h3',
       locateStrategy: 'xpath',
       index: 0
-    }
+    },
+    errorMessage: {
+      selector: '//div[@class="message"]',
+      locateStrategy: 'xpath',
+      index: 0}
   }
 };
