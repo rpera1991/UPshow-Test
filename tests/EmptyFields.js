c28fd46f4305b945524c5c3c55443c7ace0f3b77
rpera1991 <66051954+rpera1991@users.noreply.github.com>
2021-09-21 23:51:12 -0300

    Add ExtentReport Commit

--- tests/EmptyFields.js
+++ tests/EmptyFields.js
@@ -5,11 +5,16 @@ module.exports = {
        homePage.navigate();
        homePage.expect.element('@myAccountDropdown').to.be.present;
        homePage.register();
-
-      browser.windowMaximize();
-                   createAccount.createAccount();
-      var registerSuccessfully = browser.page.account();
-      registerSuccessfully.expect.element('@welcomeMessage').to.be.visible;
+       
+       var createAccount = browser.page.registration();
+       browser.windowMaximize();
+       createAccount.createAccount();
+       createAccount.expect.element('@firstNameInput').to.be.visible;
+       createAccount.expect.element('@lastNameInput').to.be.visible;
+       createAccount.expect.element('@phoneInput').to.be.visible;
+       createAccount.expect.element('@emailInput').to.be.visible;
+       createAccount.expect.element('@passwordInput').to.be.visible;
+    
     }, 
       
   };
